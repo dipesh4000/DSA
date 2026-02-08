94928d41f1e3a4d9f81cc3a1867a50bb44ed9877
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-02-08 20:12:42 +0530

    Time: 1 ms (59.97%) | Memory: 47.3 MB (68.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,23 @@
 class Solution {
-    private void zero(int[][] mat, int r, int c){
-        //rows
-        for(int i = 0; i < mat.length; i++){
-            mat[i][c] = 0;
-        }
-        //columns
-        for(int i = 0; i < mat[r].length; i++){
-            mat[r][i] = 0;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        //record is similar to a class in java but for storing records
-        record Cell(int row, int col) {}
-
-        List<Cell> zeros = new ArrayList<>();
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 0) {
-                    zeros.add(new Cell(i, j));
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean r[]=new boolean[m];
+        boolean c[]=new boolean[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    r[i]=true;
+                    c[j]=true;
                 }
             }
         }
-        for  (Cell cell : zeros) {
-            zero(matrix, cell.row, cell.col);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(r[i]||c[j]){
+                    matrix[i][j]=0;
+                }
+            }
         }
     }
 }
