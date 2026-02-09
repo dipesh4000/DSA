fe21853676ddcc81b07158279b57c55de1dc28de
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-02-09 21:00:01 +0530

    Time: 5 ms (96.60%) | Memory: 46.7 MB (88.40%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,32 +1,48 @@
 class Solution {
-    // public String removeDuplicates(String s) {
-    //     if(s.length() == 1) return s;
-    //     Stack<Character> stack = new Stack<>();
-    //     stack.push(s.charAt(0));
-    //     for(int i = 1; i < s.length(); i++){
-    //         if(!stack.empty() && stack.peek() == s.charAt(i)){
-    //             stack.pop();
-    //             continue;
-    //         }
-    //         stack.push(s.charAt(i));
-    //     }
-    //     StringBuilder sb = new StringBuilder();
-    //     while(!stack.empty()){
-    //         sb.append(stack.pop());
-    //     }
-    //     return sb.reverse().toString();
-    // }
-    public String removeDuplicates(String s) {
-    StringBuilder sb = new StringBuilder();
-    for (char c : s.toCharArray()) {
-        int len = sb.length();
-        // If the 'top' of the StringBuilder matches, 'pop' it
-        if (len > 0 && sb.charAt(len - 1) == c) {
-            sb.deleteCharAt(len - 1);
+//     // public String removeDuplicates(String s) {
+//     //     if(s.length() == 1) return s;
+//     //     Stack<Character> stack = new Stack<>();
+//     //     stack.push(s.charAt(0));
+//     //     for(int i = 1; i < s.length(); i++){
+//     //         if(!stack.empty() && stack.peek() == s.charAt(i)){
+//     //             stack.pop();
+//     //             continue;
+//     //         }
+//     //         stack.push(s.charAt(i));
+//     //     }
+//     //     StringBuilder sb = new StringBuilder();
+//     //     while(!stack.empty()){
+//     //         sb.append(stack.pop());
+//     //     }
+//     //     return sb.reverse().toString();
+//     // }
+//     public String removeDuplicates(String s) {
+//     StringBuilder sb = new StringBuilder();
+//     for (char c : s.toCharArray()) {
+//         int len = sb.length();
+//         // If the 'top' of the StringBuilder matches, 'pop' it
+//         if (len > 0 && sb.charAt(len - 1) == c) {
+//             sb.deleteCharAt(len - 1);
+//         } else {
+//             sb.append(c);
+//         }
+//     }
+//     return sb.toString();
+// }
+public String removeDuplicates(String s) {
+    char[] res = s.toCharArray();
+    int i = 0; // The 'top' of our stack
+    for (int j = 0; j < s.length(); j++) {
+        // res[j] is the current character we are looking at
+        // if stack is not empty and current matches the 'top'
+        if (i > 0 && res[i - 1] == res[j]) {
+            i--; // 'Pop' the stack
         } else {
-            sb.append(c);
+            res[i] = res[j]; // 'Push' onto the stack
+            i++;
         }
     }
-    return sb.toString();
+    return new String(res, 0, i);
 }
+
 }
