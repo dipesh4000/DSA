0581589221f8b2f0a8ba5c0b23bbb9caf9169c80
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-02-09 20:56:36 +0530

    Time: 10 ms (94.18%) | Memory: 47.4 MB (36.47%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,19 +1,32 @@
 class Solution {
+    // public String removeDuplicates(String s) {
+    //     if(s.length() == 1) return s;
+    //     Stack<Character> stack = new Stack<>();
+    //     stack.push(s.charAt(0));
+    //     for(int i = 1; i < s.length(); i++){
+    //         if(!stack.empty() && stack.peek() == s.charAt(i)){
+    //             stack.pop();
+    //             continue;
+    //         }
+    //         stack.push(s.charAt(i));
+    //     }
+    //     StringBuilder sb = new StringBuilder();
+    //     while(!stack.empty()){
+    //         sb.append(stack.pop());
+    //     }
+    //     return sb.reverse().toString();
+    // }
     public String removeDuplicates(String s) {
-        if(s.length() == 1) return s;
-        Stack<Character> stack = new Stack<>();
-        stack.push(s.charAt(0));
-        for(int i = 1; i < s.length(); i++){
-            if(!stack.empty() && stack.peek() == s.charAt(i)){
-                stack.pop();
-                continue;
-            }
-            stack.push(s.charAt(i));
+    StringBuilder sb = new StringBuilder();
+    for (char c : s.toCharArray()) {
+        int len = sb.length();
+        // If the 'top' of the StringBuilder matches, 'pop' it
+        if (len > 0 && sb.charAt(len - 1) == c) {
+            sb.deleteCharAt(len - 1);
+        } else {
+            sb.append(c);
         }
-        StringBuilder sb = new StringBuilder();
-        while(!stack.empty()){
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
     }
+    return sb.toString();
+}
 }
