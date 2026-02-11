4808b755f91ff63c23c9c42847a07a6edb0dc71b
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-02-11 19:47:10 +0530

    Time: 5 ms (97.21%) | Memory: 47.1 MB (72.45%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,30 +1,65 @@
+// class CustomStack {
+//     private int max;
+//     private ArrayList<Integer> arr;
+
+//     public CustomStack(int maxSize) {
+//         arr = new ArrayList<>(maxSize);
+//         max = maxSize;
+//     }
+    
+//     public void push(int x) {
+//         if (arr.size() < max) {
+//             arr.add(x);
+//         }
+//     }
+    
+//     public int pop() {
+//         if (arr.size() > 0) {
+//             return arr.remove(arr.size() - 1);
+//         } else {
+//             return -1;
+//         }
+//     }
+    
+//     public void increment(int k, int val) {
+//         int limit = Math.min(k, arr.size());
+//         for (int i = 0; i < limit; i++) {
+//             arr.set(i, arr.get(i) + val);
+//         }
+//     }
+// }
 class CustomStack {
-    private int max;
-    private ArrayList<Integer> arr;
+
+    int maxSize;
+    int[] stack;
+    int top;
 
     public CustomStack(int maxSize) {
-        arr = new ArrayList<>(maxSize);
-        max = maxSize;
+        this.maxSize = maxSize;
+        this.stack = new int[maxSize];
+        this.top = -1;
     }
     
     public void push(int x) {
-        if (arr.size() < max) {
-            arr.add(x);
+        if (top < maxSize - 1) {
+            top++;
+            stack[top] = x;
         }
     }
     
     public int pop() {
-        if (arr.size() > 0) {
-            return arr.remove(arr.size() - 1);
-        } else {
+        if (top == -1) {
             return -1;
         }
+        int res = stack[top];
+        top--;
+        return res;
     }
     
     public void increment(int k, int val) {
-        int limit = Math.min(k, arr.size());
-        for (int i = 0; i < limit; i++) {
-            arr.set(i, arr.get(i) + val);
+        int n = Math.min(k, top + 1);
+        for (int i = 0; i < n; i++) {
+            stack[i] += val;
         }
     }
 }
