be5db9538ebfdfec42faec17986ec8f0066ba3bf
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-02-14 21:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.69%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
@@ -8,17 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public int getDecimalValue(ListNode head) {
+//         if(head.next == null) return head.val;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         while(head != null){
+//             list.add(head.val);
+//             head = head.next;
+//         }
+//         int ans = 0;
+//         for(int i = list.size() - 1; i >= 0; i--){
+//             ans += (list.get(i))*(int)(Math.pow(2, (list.size() - 1) - i));
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int getDecimalValue(ListNode head) {
-        if(head.next == null) return head.val;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(head != null){
-            list.add(head.val);
-            head = head.next;
-        }
         int ans = 0;
-        for(int i = list.size() - 1; i >= 0; i--){
-            ans += (list.get(i))*(int)(Math.pow(2, (list.size() - 1) - i));
+        while (head != null) {
+            ans = ans * 2 + head.val;
+            head = head.next;
         }
         return ans;
     }
