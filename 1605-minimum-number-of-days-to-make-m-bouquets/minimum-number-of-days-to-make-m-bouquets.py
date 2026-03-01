2dadac399fe5972d7bc239edf081fe8383da6c8f
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-03-01 12:46:54 +0530

    Time: 260 ms (96.99%) | Memory: 31.7 MB (80.95%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,30 +1,34 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        l, r = min(bloomDay), max(bloomDay)
-
-        if(len(bloomDay) < (m*k)):
+        if len(bloomDay) < m * k:
             return -1
-        if(len(bloomDay) == k):
-            return 1
-        while(l<r):
-            mid = (int)(l + (r-l)//2)
-            if(self.candone(bloomDay, m, k, mid)):
-                r = mid
-            else:
-                l = mid + 1
         
-        return r
-    def candone(self, bloomday: List[int], m: int, k: int, mid: int) -> bool:
-        boq_made = 0
-        flowers = 0
-
-        for bloom in bloomday:
-            if(bloom>mid):
-                flowers = 0
-            elif(flowers<k):
-                flowers += 1
+        left, right = 1, max(bloomDay)
+        
+        while left < right:
+            mid = left + (right - left) // 2
             
-            if(flowers == k):
-                boq_made += 1
+            if self.canMake(bloomDay, m, k, mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+    
+    def canMake(self, bloomDay, m, k, day):
+        bouquets = 0
+        flowers = 0
+        
+        for bloom in bloomDay:
+            if bloom <= day:
+                flowers += 1
+                if flowers == k:
+                    bouquets += 1
+                    flowers = 0
+                    
+                    if bouquets == m:   # 🚀 Early exit
+                        return True
+            else:
                 flowers = 0
-        return boq_made >= m
+                
+        return False
