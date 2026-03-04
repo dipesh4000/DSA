a6b15b50ce510c8fbb403a2b1c1a36fd50baf0d6
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-03-04 22:59:15 +0530

    Time: 8 ms (33.79%) | Memory: 26.5 MB (84.28%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,26 @@
+# class Solution:
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         l = len(nums)
+#         k %= l
+#         nums += nums[l-k:]
+#         nums += nums[:l-k]
+#         nums[:] = nums[l:]
+
+        ##optimal solution
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k, n-1)
