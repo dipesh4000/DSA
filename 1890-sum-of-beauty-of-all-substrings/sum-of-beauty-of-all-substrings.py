7c04691fea596d948b7ca708fdfcc72ccba37559
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-03-29 23:49:13 +0530

    Time: 1160 ms (89.56%) | Memory: 19.2 MB (74.82%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -1,14 +1,13 @@
-from collections import Counter
-
 class Solution:
     def beautySum(self, s: str) -> int:
         n = len(s)
-        ans = 0
-        
+        res = 0
+
         for i in range(n):
+            freq = {}
             for j in range(i, n):
-                sub = s[i:j+1]
-                freq = Counter(sub).values()
-                ans += max(freq) - min(freq)
-                
-        return ans
+                freq[s[j]] = freq.get(s[j], 0) + 1
+                values = freq.values()
+                res += max(values) - min(values)
+
+        return res
