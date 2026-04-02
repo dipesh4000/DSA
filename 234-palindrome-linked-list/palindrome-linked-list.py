c788bd4e728602f968820420b641c0089ea35f05
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-04-02 07:02:22 +0530

    Time: 54 ms (13.54%) | Memory: 37.9 MB (98.23%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,45 +5,49 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if head == None or head.next == None:
+        if not head or not head.next:
             return True
-        linked = []
-        while(head):
-            linked.append(head.val)
-            head = head.next
-        if(len(linked)%2 == 0):
-            mid = len(linked)//2
-            left, right = mid -1, mid
-            while(left >= 0 and right < len(linked)):
-                if(linked[left] != linked[right]):
-                    return False
-                left -= 1
-                right += 1
-            return True
-        else:
-            mid = len(linked)//2
-            left, right = mid -1, mid + 1
-            while(left >= 0 and right < len(linked)):
-                if(linked[left] != linked[right]):
-                    return False
-                left -= 1
-                right += 1
-            return True
-        return False
         
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            
+            nxt = slow.next      
+            slow.next = prev     
+            prev = slow
+            slow = nxt
+        if fast:
+            slow = slow.next
+        while prev and slow:
+            if prev.val != slow.val:
+                return False
+            prev = prev.next
+            slow = slow.next
+        
+        return True
         # if head == None or head.next == None:
-        #     return False
-        # slow, fast, initial = head, head, head
-        # prev = None
-        # while(fast and fast.next):
-        #     temp = slow 
-        #     temp.next = prev
-        #     prev = temp
-        #     slow = slow.next
-        #     fast = fast.next.next
-        # while(slow):
-        #     if(slow.val != initial.val):
-        #         return False
-        #     slow = slow.next
-        #     intial = initial.next
-        # return True
+        #     return True
+        # linked = []
+        # while(head):
+        #     linked.append(head.val)
+        #     head = head.next
+        # if(len(linked)%2 == 0):
+        #     mid = len(linked)//2
+        #     left, right = mid -1, mid
+        #     while(left >= 0 and right < len(linked)):
+        #         if(linked[left] != linked[right]):
+        #             return False
+        #         left -= 1
+        #         right += 1
+        #     return True
+        # else:
+        #     mid = len(linked)//2
+        #     left, right = mid -1, mid + 1
+        #     while(left >= 0 and right < len(linked)):
+        #         if(linked[left] != linked[right]):
+        #             return False
+        #         left -= 1
+        #         right += 1
+        #     return True
+        # return False
