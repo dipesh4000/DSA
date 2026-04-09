dd42fb0ab8cb34c652d258914239ca2a15411a74
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-04-09 22:43:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (90.44%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,10 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def helper(x, n):
-            if x == 0: return 0
-            if n == 0: return 1
+        if n == 0:
+            return 1
+        if n < 0:
+            return self.myPow(1/x, -n)
+    
+        if n % 2 == 0:
+            half = self.myPow(x, n//2)
+            return half * half        # reuse, don't call twice!
+        else:
+            return x * self.myPow(x, n-1)
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         def helper(x, n):
+#             if x == 0: return 0
+#             if n == 0: return 1
 
-            res = helper(x*x, n//2)
-            return x*res if n % 2 else res
-        res = helper(x, abs(n))
-        return res if n>=0 else 1/res
+#             res = helper(x*x, n//2)
+#             return x*res if n % 2 else res
+#         res = helper(x, abs(n))
+#         return res if n>=0 else 1/res
