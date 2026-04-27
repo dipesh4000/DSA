5ab721ef2c27605617e54d7921c99ea9a11cd910
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-04-27 21:03:25 +0530

    Time: 39 ms (79.74%) | Memory: 32.3 MB (99.26%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,32 +1,35 @@
-class Solution(object):
-    @cache  # the memory trick can save some time
-    def partition(self, s):
-        if not s: return [[]]
-        ans = []
-        for i in range(1, len(s) + 1):
-            if s[:i] == s[:i][::-1]:  # prefix is a palindrome
-                for suf in self.partition(s[i:]):  # process suffix recursively
-                    ans.append([s[:i]] + suf)
-        return ans
-# class Solution:
-#     def partition(self, s: str) -> List[List[str]]:
-#         res = []
-#         part = []
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        self.n = len(s)
+        res = []
+        self.backtrack(s, 0, [], res)
+        return res
 
-#         def dfs(i):
-#             if i >= len(s):
-#                 res.append(part.copy())
-#                 return
-#             for j in range(i, len(s)):
-#                 if self.isPal(s, i, j):
-#                     part.append(s[i:j+1])
-#                     dfs(j + 1)
-#                     part.pop()
-#         dfs(0)
-#         return res
-#     def isPal(self, s, l, r):
-#         while l < r:
-#             if s[1] != s[r]:
-#                 return False
-#             l, r = l + 1, r - 1
-#         return True
+    def backtrack(self, s, idx, path, res):
+        if idx == self.n:
+            res.append(path[:])
+            return
+
+        for i in range(idx, len(s)):
+            if self.is_palindrome(s, idx, i):
+                path.append(s[idx:i+1])
+                self.backtrack(s, i+1, path, res)
+                path.pop()
+
+    def is_palindrome(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+# class Solution(object):
+#     @cache  # the memory trick can save some time
+#     def partition(self, s):
+#         if not s: return [[]]
+#         ans = []
+#         for i in range(1, len(s) + 1):
+#             if s[:i] == s[:i][::-1]:  # prefix is a palindrome
+#                 for suf in self.partition(s[i:]):  # process suffix recursively
+#                     ans.append([s[:i]] + suf)
+#         return ans
