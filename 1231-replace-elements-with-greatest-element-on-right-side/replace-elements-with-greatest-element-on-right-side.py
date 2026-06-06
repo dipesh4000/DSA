80b3a45a9b92a84b186976ebb47d4ed14de24a07
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-06 08:27:02 +0530

    Time: 12 ms (92.31%) | Memory: 20.1 MB (99.74%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,18 @@
+# class Solution:
+#     def replaceElements(self, arr: List[int]) -> List[int]:
+#         ans = [-1]*len(arr)
+#         greatest_seen = -1
+#         for i in range(len(arr) - 1, -1, -1):
+#             ans[i] = greatest_seen
+#             if(arr[i] > greatest_seen):
+#                 greatest_seen = arr[i]
+#         return ans
+
+
 class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        ans = [-1]*len(arr)
+    def replaceElements(self, arr: list[int]) -> list[int]:
         greatest_seen = -1
         for i in range(len(arr) - 1, -1, -1):
-            ans[i] = greatest_seen
-            if(arr[i] > greatest_seen):
-                greatest_seen = arr[i]
-        return ans
+            arr[i], greatest_seen = greatest_seen, max(greatest_seen, arr[i])
+
+        return arr
