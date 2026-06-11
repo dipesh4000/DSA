292103ddeab473e766a0a7c1c87a2b295725fa27
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-11 22:30:21 +0530

    Time: 125 ms (45.30%) | Memory: 31.3 MB (45.65%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,33 +1,55 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-
-        left = [0] * n
-        right = [0] * n
-
-        stack = []
-
-        # left boundary
-        for i in range(n):
-            while stack and heights[stack[-1]] > heights[i]:
-                stack.pop()
-
-            left[i] = stack[-1] + 1 if stack else 0
-            stack.append(i)
-
-        stack.clear()
-
-        # right boundary
-        for i in range(n - 1, -1, -1):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-
-            right[i] = stack[-1] if stack else n
-            stack.append(i)
-
+        stack = [-1]
         ans = 0
 
-        for i in range(n):
-            ans = max(ans, heights[i] * (right[i] - left[i]))
+        for i, h in enumerate(heights):
+            while stack[-1] != -1 and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i - stack[-1] - 1
+                ans = max(ans, height * width)
+
+            stack.append(i)
+
+        n = len(heights)
+
+        while stack[-1] != -1:
+            height = heights[stack.pop()]
+            width = n - stack[-1] - 1
+            ans = max(ans, height * width)
 
         return ans
+
+# class Solution:
+#     def largestRectangleArea(self, heights: List[int]) -> int:
+#         n = len(heights)
+
+#         left = [0] * n
+#         right = [0] * n
+
+#         stack = []
+
+#         # left boundary
+#         for i in range(n):
+#             while stack and heights[stack[-1]] > heights[i]:
+#                 stack.pop()
+
+#             left[i] = stack[-1] + 1 if stack else 0
+#             stack.append(i)
+
+#         stack.clear()
+
+#         # right boundary
+#         for i in range(n - 1, -1, -1):
+#             while stack and heights[stack[-1]] >= heights[i]:
+#                 stack.pop()
+
+#             right[i] = stack[-1] if stack else n
+#             stack.append(i)
+
+#         ans = 0
+
+#         for i in range(n):
+#             ans = max(ans, heights[i] * (right[i] - left[i]))
+
+#         return ans
