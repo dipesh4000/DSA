cb0a216c6979c084a307a9f1565a046584e530ac
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-15 10:47:38 +0530

    Time: 66 ms (98.23%) | Memory: 25.9 MB (34.52%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,20 +1,47 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        basket = {}
-        i = 0
-        maxfruit = 0
 
-        for j in range(len(fruits)):
-            basket[fruits[j]] = basket.get(fruits[j], 0) + 1
+        last = -1
+        second_last = -1
 
-            while len(basket) > 2:
-                basket[fruits[i]] -= 1
+        last_count = 0
+        curr = 0
+        best = 0
 
-                if basket[fruits[i]] == 0:
-                    del basket[fruits[i]]
+        for fruit in fruits:
 
-                i += 1
+            if fruit == last or fruit == second_last:
+                curr += 1
+            else:
+                curr = last_count + 1
 
-            maxfruit = max(maxfruit, j - i + 1)
+            if fruit == last:
+                last_count += 1
+            else:
+                last_count = 1
+                second_last = last
+                last = fruit
 
-        return maxfruit
+            best = max(best, curr)
+
+        return best
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+#         basket = {}
+#         i = 0
+#         maxfruit = 0
+
+#         for j in range(len(fruits)):
+#             basket[fruits[j]] = basket.get(fruits[j], 0) + 1
+
+#             while len(basket) > 2:
+#                 basket[fruits[i]] -= 1
+
+#                 if basket[fruits[i]] == 0:
+#                     del basket[fruits[i]]
+
+#                 i += 1
+
+#             maxfruit = max(maxfruit, j - i + 1)
+
+#         return maxfruit
