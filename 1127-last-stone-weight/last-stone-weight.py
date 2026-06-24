239b21ea774612172d992c82d09665408b9257ba
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-24 23:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (35.18%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,15 +1,31 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        for i in range(len(stones)):
-            stones[i] = -stones[i]
-        heapq.heapify(stones)
-        while len(stones) > 1:
-            largest = heapq.heappop(stones)
-            next_largest = heapq.heappop(stones)
+        maxHeap = [-stone for stone in stones]
+        heapq.heapify(maxHeap)
 
-            if largest != next_largest:
-                heapq.heappush(stones, largest - next_largest)
+        while len(maxHeap) > 1:
+            y = -1 * heapq.heappop(maxHeap)
+            x = -1 * heapq.heappop(maxHeap)
+            if x != y:
+                heapq.heappush(maxHeap, -1 * (y - x))
 
-        if len(stones) == 1:
-            return -heapq.heappop(stones)
-        return 0
+        if len(maxHeap) < 1:
+            return 0
+        return -1 * maxHeap[-1]
+
+
+# class Solution:
+#     def lastStoneWeight(self, stones: List[int]) -> int:
+#         for i in range(len(stones)):
+#             stones[i] = -stones[i]
+#         heapq.heapify(stones)
+#         while len(stones) > 1:
+#             largest = heapq.heappop(stones)
+#             next_largest = heapq.heappop(stones)
+
+#             if largest != next_largest:
+#                 heapq.heappush(stones, largest - next_largest)
+
+#         if len(stones) == 1:
+#             return -heapq.heappop(stones)
+#         return 0
