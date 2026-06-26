cdb32933548fc555b081b85cd99d60836f79f9f2
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-26 20:01:37 +0530

    Time: 3085 ms (5.01%) | Memory: 22.5 MB (93.09%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while True:
+            min_val = float("inf")
+            min_node = None
+            min_index = -1
+
+            for i in range(0, len(lists)):
+                if lists[i] and lists[i].val < min_val:
+                    min_val = lists[i].val
+                    min_node = lists[i]
+                    min_index = i
+
+            if min_node == None:
+                break
+
+            tail.next = min_node
+            tail = tail.next
+
+            lists[min_index] = lists[min_index].next
+
+        tail.next = None
+        return dummy.next
+# curr[i] = head of every linked list
+
+# dummy = new ListNode()
+# tail = dummy
+
+# while(true):
+
+#     minNode = null
+#     minIndex = -1
+
+#     for i = 0 to k-1:
+#         if curr[i] != null:
+#             if minNode == null OR curr[i].val < minNode.val:
+#                 minNode = curr[i]
+#                 minIndex = i
+
+#     if minNode == null:
+#         break          // all lists exhausted
+
+#     tail.next = minNode
+#     tail = tail.next
+
+#     curr[minIndex] = curr[minIndex].next
+
+# tail.next = null
+# return dummy.next
