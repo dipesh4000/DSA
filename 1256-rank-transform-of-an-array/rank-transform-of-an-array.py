00d85cea3f20746a14be6fd1377a975277960199
Dipesh <87355477+dipesh4000@users.noreply.github.com>
2026-06-27 07:54:12 +0530

    Time: 49 ms (40.64%) | Memory: 37.4 MB (79.92%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,13 +1,21 @@
 class Solution:
     def arrayRankTransform(self, arr):
-        nums = sorted(arr)
-
         rank = {}
-        current = 1
 
-        for num in nums:
-            if num not in rank:
-                rank[num] = current
-                current += 1
+        for i, num in enumerate(sorted(set(arr)), 1):
+            rank[num] = i
 
         return [rank[num] for num in arr]
+# class Solution:
+#     def arrayRankTransform(self, arr):
+#         nums = sorted(arr)
+
+#         rank = {}
+#         current = 1
+
+#         for num in nums:
+#             if num not in rank:
+#                 rank[num] = current
+#                 current += 1
+
+#         return [rank[num] for num in arr]
